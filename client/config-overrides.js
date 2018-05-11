c67f9b7e38cb0ced6d51ba254b9462e1ed4ae908
Dung Lam <lhdung1997@gmail.com>
2018-05-11 18:16:48 +0700

    Add RegisterForm

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,12 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const rewireReactHotLoader = require("react-app-rewire-hot-loader");
+const { injectBabelPlugin } = require("react-app-rewired");
 
 /* config-overrides.js */
-module.exports = function override (config, env) {
-  config = rewireReactHotLoader(config, env)
-  return config
-}
+module.exports = function override(config, env) {
+  config = rewireReactHotLoader(config, env);
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
+    config
+  );
+  return config;
+};
